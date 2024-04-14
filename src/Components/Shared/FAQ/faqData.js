c3229b67fe81d239aeaa3a9bbbe1ea35cb6c0c32
divyanshu_sahu <divyanshusahucoc@gmail.com>
2024-04-14 21:49:34 +0530

    build Faq component

---
+++ src/Components/Shared/FAQ/faqData.js
@@ -0,0 +1,30 @@
+const faqData = [
+  {
+    question: "What is the purpose of this challenge?",
+    answer:
+      "This challenge aims to enhance our skills in React.js by creating 30 different components for coding practice and displaying them on a website",
+  },
+  {
+    question: "How can I participate in this challenge?",
+    answer:
+      "This challenge aims to enhance our skills in React.js by creating 30 different components for coding practice and displaying them on a website",
+  },
+  {
+    question: "Are there any prizes for completing the challenge?",
+    answer:
+      "This challenge aims to enhance our skills in React.js by creating 30 different components for coding practice and displaying them on a website",
+  },
+  {
+    question: "How will the components be displayed on the website?",
+    answer:
+      "This challenge aims to enhance our skills in React.js by creating 30 different components for coding practice and displaying them on a website",
+  },
+  {
+    question:
+      "Can I use libraries or frameworks other than React.js  challenge?",
+    answer:
+      "This challenge aims to enhance our skills in React.js by creating 30 different components for coding practice and displaying them on a website",
+  },
+];
+
+export default faqData;
