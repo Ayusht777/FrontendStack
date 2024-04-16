02c3f653c3ab67eb621749e2570b434e3dadd43e
unique011 <harshitsharma.2021cs@technonjr.org>
2024-04-16 18:33:33 +0530

    style(heading):change style

--- src/Components/Shared/Heading.jsx
+++ src/Components/Shared/Heading.jsx
@@ -1,9 +1,16 @@
-function Heading({ content }) {
+const Heading = ({ title, type = "main", className = "" }) => {
+  const head = {
+    main: "md:text-5xl text-3xl font-extrabold",
+    sub: "md:text-5xl text-3xl font-extrabold",
+  };
   return (
-    <h1 className="md:text-5xl text-3xl font-bold text-center bg-gradient-to-r from-white to-white/70 text-transparent bg-clip-text tracking-tighter" aria-label={content}>
-      {content}
+    <h1
+      className={`text-center bg-gradient-to-r from-white to-white/70 text-transparent bg-clip-text tracking-tighter !leading-[5rem] ${head[type]} ${className}`}
+      aria-label={title}
+    >
+      {title}
     </h1>
   );
-}
+};
 
 export default Heading;
