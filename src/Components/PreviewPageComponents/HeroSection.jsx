80e906101cc6e7c6b5197439cc230a4e228d879c
Ayusht777 <talesarayush@gmail.com>
2024-04-18 16:41:13 +0530

    feat: Update preview components and styles

---
+++ src/Components/PreviewPageComponents/HeroSection.jsx
@@ -0,0 +1,12 @@
+const HeroSection = () => {
+  return (
+    <section className="md:w-4/5 flex justify-center items-center lg:h-[480px] md:h-[420px] h-[320px] flex-col my-4">
+      <span className=" max-w-5xl  text-center bg-gradient-to-r from-white to-white/70 text-transparent bg-clip-text tracking-tighter lg:text-7xl md:text-5xl text-3xl font-semibold md:py-1.5  ">
+        Explore the Live Preview and Visualize the Code
+      </span>
+
+    </section>
+  );
+};
+
+export default HeroSection;
