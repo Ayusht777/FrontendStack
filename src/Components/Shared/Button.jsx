bf8da2fea2d2a8aa317bd4feadea58eb9d35edc6
unique011 <harshitsharma.2021cs@technonjr.org>
2024-04-19 22:19:57 +0530

    refactor(Button):make it friendly to use and customized.

--- src/Components/Shared/Button.jsx
+++ src/Components/Shared/Button.jsx
@@ -1,11 +1,14 @@
-const Button = ({ label = "Label", width = "w-48", bgColor = "Logo" ,textColor="text-white"}) => {
+const Button = ({ className = "", type = "primary", children, ...props }) => {
+  const types = {
+    primary: "focus-visible:outline-Logo bg-Logo",
+    secondary: "focus-visible:outline-Border bg-Border text-white",
+  };
   return (
     <button
-      className={`h-10 rounded-lg font-semibold focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-4 ${
-        `focus-visible:outline-` + bgColor
-      } ${width} ${`bg-` + bgColor} ${textColor}`}
+      className={`h-8 w-32 text-base md:h-10  md:w-48 md:text-lg  rounded-lg font-semibold focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-4  ${types[type]}  ${className}`}
+      {...props}
     >
-      {label}
+      {children}
     </button>
   );
 };
