c1f109f0a5a00c74dc9fe16f75e590c74c447a12
Ayusht777 <talesarayush@gmail.com>
2024-04-22 16:15:21 +0530

    feat: Create Toolbar component with back button and dropdown

---
+++ src/Components/PreviewPageComponents/ToolbarSection.jsx
@@ -0,0 +1,16 @@
+import { ArrowLeft } from "lucide-react";
+import { Link } from "react-router-dom";
+import DropdownMenu from "@/Components/PreviewPageComponents/DropdownMenuComponent/DropdownMenu";
+const ToolbarSection = () => {
+  return (
+    <section className="md:w-4/5 w-full flex justify-between items-start my-4">
+      {/* <button className="size-7 bg-Nav rounded-md text-Logo "><Link to="/gallery"><ArrowLeft strokeWidth="2" className="mx-auto "/></Link></button> */}
+      <button className="md:size-9 size-7 bg-Nav rounded-md text-Logo">
+        <ArrowLeft strokeWidth="2" className="mx-auto " />
+      </button>
+      <DropdownMenu></DropdownMenu>
+    </section>
+  );
+};
+
+export default ToolbarSection;
