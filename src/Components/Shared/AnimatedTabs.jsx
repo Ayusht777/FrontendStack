1789482eb7e7ab46021ad976c2f039f0ffe98349
Ayusht777 <talesarayush@gmail.com>
2024-04-24 19:28:15 +0530

    feat: Enhance activetabs UI and logic for better management

--- src/Components/Shared/AnimatedTabs.jsx
+++ src/Components/Shared/AnimatedTabs.jsx
@@ -13,15 +13,14 @@ const AnimatedTabs = ({ Tabs, onActiveTabIndexChange }) => {
       const currentTab = tabsRef.current[activeTabIndex];
       setTabUnderlineLeft(currentTab?.offsetLeft ?? 0);
       setTabUnderlineWidth(currentTab?.clientWidth ?? 0);
-      onActiveTabIndexChange(activeTabIndex);
-      console.log(activeTabIndex)
+      onActiveTabIndexChange(Tabs[activeTabIndex].id);
     };
 
     setTabPosition();
   }, [activeTabIndex]);
 
   return (
-    <div className=" flex-row relative mx-auto flex w-full justify-center h-12 rounded-t-xl  bg-Border px-2 backdrop-blur-sm">
+    <div className=" flex-row relative mx-auto flex w-full  h-12 bg-Border  backdrop-blur-sm rounded-xl justify-evenly px-1">
       <span
         className="absolute bottom-0 top-0 -z-10 flex overflow-hidden rounded-xl py-[.30rem] transition-all duration-300"
         style={{ left: tabUnderlineLeft, width: tabUnderlineWidth }}
@@ -37,7 +36,7 @@ const AnimatedTabs = ({ Tabs, onActiveTabIndexChange }) => {
             ref={(el) => (tabsRef.current[index] = el)}
             className={`${
               isActive ? "text-Logo" : " text-white"
-            } my-auto cursor-pointer select-none rounded-full w-full text-center font-bold`}
+            } my-auto cursor-pointer select-none rounded-full  text-center font-semibold px-2 w-full`}
             onClick={() => setActiveTabIndex(index)}
           >
             {tab.name}
