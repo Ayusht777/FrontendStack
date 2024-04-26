f4ce40351ac932d1f6423f5f954f03353f5b5459
unique011 <harshitsharma.2021cs@technonjr.org>
2024-04-26 22:40:03 +0530

    fix layout of about us page

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -8,7 +8,7 @@ const About = () => {
   return (
     <div className="w-full bg-Bg  flex flex-col justify-start items-center min-h-screen space-y-32">
       <Navbar></Navbar>
-      <section className="text-center text-white space-y-8">
+      <section className="text-center text-white space-y-5">
         <Heading title={"About Us"} />
         <p className="w-[70%] text-justify text-lg md:text-xl leading-7 md:leading-10 mx-auto font-normal md:font-medium">
           Welcome to our corner of the web! We are a group of passionate
@@ -17,7 +17,7 @@ const About = () => {
           functional components that elevate user experiences on the web.
         </p>
       </section>
-      <div className="text-center text-white space-y-8">
+      <div className="text-center text-white space-y-5">
         <Heading title={"Why we take this challenge?"} type="sub" />
         <p className=" w-4/5 md:w-[70%] text-justify text-lg md:text-xl leading-7 md:leading-10 mx-auto font-normal md:font-medium">
           In the past, we have completed three rounds of the
