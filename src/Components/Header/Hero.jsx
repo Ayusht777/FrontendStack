a23c9bcda63244ba012a92e8e28c28caba813635
unique011 <harshitsharma.2021cs@technonjr.org>
2024-04-26 22:41:06 +0530

    Fix(Hero): create responsive Hero section

--- src/Components/Header/Hero.jsx
+++ src/Components/Header/Hero.jsx
@@ -6,12 +6,18 @@ const Hero = () => {
     <div className="w-4/5 h-[calc(100vh-7rem)] flex justify-center items-center flex-col space-y-8">
       <Heading
         title={`Showcasing Our Front-end `}
-        className="!text-7xl"
-        type="main"
+        className="!text-4xl md:!text-7xl leading-9 md:!leading-[5.5rem]"
+        type=""
       />
-      <Heading title={`Components For You`} type="sub" className="!text-7xl" />
-      <h2 className="text-4xl text-white">Exclusive High-Quality Parts</h2>
-      <div className="flex justify-center items-center gap-7">
+      <Heading
+        title={`Components For You`}
+        type=""
+        className="!text-4xl md:!text-7xl leading-9 md:!leading-[5.5rem]"
+      />
+      <h2 className="text-xl sm:text-4xl text-white text-center">
+        Exclusive High-Quality Parts
+      </h2>
+      <div className="flex justify-center items-center gap-2 md:gap-7">
         <Button>Get Started</Button>
         <Button
           className="flex justify-center items-center gap-3"
--- src/Components/Shared/Heading.jsx
+++ src/Components/Shared/Heading.jsx
@@ -1,11 +1,13 @@
 const Heading = ({ title, type = "main", className = "" }) => {
   const head = {
-    main: "md:text-5xl text-3xl font-extrabold",
-    sub: "md:text-5xl text-3xl  font-bold md:mb-32 mb-12",
+    main: "md:text-5xl text-4xl font-extrabold",
+    sub: "md:text-4xl text-3xl  font-bold ",
   };
   return (
     <h1
-      className={`text-center bg-gradient-to-b from-white to-white/70 text-transparent bg-clip-text tracking-tighter !leading-[5.5rem] ${head[type]} ${className}`}
+      className={`text-center bg-gradient-to-b from-white  to-white/70 text-transparent bg-clip-text tracking-tighter leading-[5.5rem] ${
+        type && head[type]
+      } ${className}`}
       aria-label={title}
     >
       {title}
