b2ffe6b65f97a5451f3e77252fcd73c0ee0fdea6
Ayusht777 <talesarayush@gmail.com>
2024-04-27 19:28:15 +0530

    style(HeroSection.jsx): Update width from md:w-4/5 to w-full

--- src/Components/PreviewPageComponents/HeroSection.jsx
+++ src/Components/PreviewPageComponents/HeroSection.jsx
@@ -1,10 +1,9 @@
-const HeroSection = ({Title}) => {
+const HeroSection = ({ Title }) => {
   return (
-    <section className="md:w-4/5 flex justify-center items-center lg:h-[480px] md:h-[420px] h-[320px] flex-col my-4">
-      <span className=" max-w-5xl  text-center bg-gradient-to-r from-white to-white/70 text-transparent bg-clip-text tracking-tighter lg:text-7xl md:text-5xl text-3xl font-semibold md:py-1.5  ">
-       {Title}
+    <section className=" w-full flex justify-center items-center lg:h-[480px] md:h-[420px] h-[320px] flex-col my-4">
+      <span className="max-w-5xl   text-center bg-gradient-to-r from-white to-white/70 text-transparent bg-clip-text tracking-tighter lg:text-7xl md:text-5xl text-3xl font-semibold md:py-1.5  ">
+        {Title}
       </span>
-
     </section>
   );
 };
