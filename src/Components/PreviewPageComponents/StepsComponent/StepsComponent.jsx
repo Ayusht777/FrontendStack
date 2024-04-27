6b85f9826a904cda2ee7c17d093920678a19aa9e
Ayusht777 <talesarayush@gmail.com>
2024-04-27 19:31:38 +0530

    refactor(StepsComponent) : Improved StepsComponent rendering logic

--- src/Components/PreviewPageComponents/StepsComponent/StepsComponent.jsx
+++ src/Components/PreviewPageComponents/StepsComponent/StepsComponent.jsx
@@ -4,14 +4,14 @@ import SyntaxHighlighter from "@/Components/PreviewPageComponents/SyntaxHighligh
 import { useState } from "react";
 
 const CommandStep = ({ command, isOptional }) => (
-  <li className="px-2 py-1.5 flex items-start relative">
+  <li className="relative flex items-start px-2 py-1.5">
     {isOptional ? (
       <span className="pr-1 text-[#B392F0]"> $ </span>
     ) : (
-      <span className="block rounded-full size-2 bg-blue-400 my-auto mr-2"></span>
+      <span className="my-auto mr-2 block size-2 rounded-full bg-blue-400"></span>
     )}
     {isOptional ? (
-      <span className=" overflow-auto lg:basis basis-5/6">
+      <span className=" lg:basis basis-5/6 overflow-auto">
         <SyntaxHighlighter
           CodeSnippets={`    ` + command}
           Language="shell"
@@ -19,13 +19,13 @@ const CommandStep = ({ command, isOptional }) => (
         />
       </span>
     ) : (
-      <span className="text-blue-400 overflow-auto basis-5/6">{command}</span>
+      <span className="basis-5/6 overflow-auto text-blue-400">{command}</span>
     )}
     {isOptional ? (
       <CopyToClipBoard textToCopy={command} placement={false} />
     ) : (
       <a href={command} target="_blank" rel="noopener noreferrer">
-        <Link className="text-Logo size-4 absolute z-20 right-3" />
+        <Link className="absolute right-3 z-20 size-4 text-Logo" />
       </a>
     )}
   </li>
@@ -33,24 +33,24 @@ const CommandStep = ({ command, isOptional }) => (
 
 const StepsComponent = ({ Data }) => {
   const [isCommandSectionOpen, setIsCommandSectionOpen] = useState(false);
-  console.log(Data.commands);
+
   return (
-    <section className="w-full bg-Nav h-auto flex justify-center items-center flex-col rounded-lg divide-y divide-Bg  md:my-6 my-4">
+    <section className="my-4 flex h-auto w-full max-w-[1200px] flex-col items-center justify-center divide-y divide-Bg rounded-lg  bg-Nav md:my-6">
       <button
-        className="w-full flex items-center justify-between px-2 py-1.5 rounded-t-lg"
+        className="flex w-full items-center justify-between rounded-t-lg px-2 py-1.5"
         onClick={() => setIsCommandSectionOpen(!isCommandSectionOpen)}
       >
-        <p className=" w-full text-white font-semibold text-start md:tracking-normal tracking-tight md:text-base text-sm">
+        <p className=" w-full text-start text-sm font-semibold tracking-tight text-white md:text-base md:tracking-normal">
           {Data.title}
         </p>
         <ChevronDown
           className={`text-Logo ${
             isCommandSectionOpen ? "rotate-180" : ""
-          } transform ease-in-out duration-300`}
+          } transform duration-300 ease-in-out`}
         />
       </button>
       {isCommandSectionOpen && (
-        <ul className={`w-full block`}>
+        <ul className={`block w-full`}>
           {Data.commands.map((item, index) => (
             <CommandStep
               key={index}
