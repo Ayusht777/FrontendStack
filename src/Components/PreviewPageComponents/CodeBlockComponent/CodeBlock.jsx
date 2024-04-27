95d2ec16b4aa2d44169f89340afa8c0ab59a709e
Ayusht777 <talesarayush@gmail.com>
2024-04-27 19:35:20 +0530

    refactor: Improved CodeBlock component structure

--- src/Components/PreviewPageComponents/CodeBlockComponent/CodeBlock.jsx
+++ src/Components/PreviewPageComponents/CodeBlockComponent/CodeBlock.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import SyntaxHighlighter from "@/Components/PreviewPageComponents/SyntaxHighlighter";
 import AnimatedTabs from "@/Components/Shared/AnimatedTabs";
 import CopyToClipBoard from "@/Utils/CopyToClipBoard";
@@ -7,9 +7,9 @@ const ComponentPreview = ({ PreviewComponent, isVisible }) => {
   const RenderedComponent = PreviewComponent;
   return (
     <section
-      className={`w-auto max-h-[520px] h-[520px] overflow-auto md:p-4 p-2 scroll-thin bg-gradient-to-tr from-Nav to-Nav/10 ${
+      className={`scroll-thin h-auto max-h-[520px] min-h-56 w-auto overflow-auto rounded-lg bg-gradient-to-tr from-Nav to-Nav/10 p-2 md:p-4 ${
         isVisible ? "hidden" : "flex"
-      } justify-center items-start`}
+      } items-center justify-center`}
     >
       <RenderedComponent />
     </section>
@@ -19,30 +19,33 @@ const CodeBlock = ({ CodeSnippets, PreviewComponent }) => {
   const TabData = [
     { id: "preview", name: "PREVIEW" },
     { id: "code", name: "CODE" },
-  ]
+  ];
   const [activeTabIndex, setActiveTabIndex] = useState(TabData[0].id);
   const handleActiveTabIndexChange = (newIndex) => {
     setActiveTabIndex(newIndex);
-    console.log(activeTabIndex);
   };
   return (
     <>
-      <section className="md:w-4/5 w-full bg-Nav rounded-lg relative my-4">
+      <section className="relative my-4 w-full max-w-[1200px] rounded-lg bg-Nav ">
         <AnimatedTabs
           Tabs={TabData}
           onActiveTabIndexChange={handleActiveTabIndexChange}
         />
 
-        <ComponentPreview
-          PreviewComponent={PreviewComponent}
-          isVisible={activeTabIndex === "preview" ? false : true}
-        />
+        {PreviewComponent && (
+          <ComponentPreview
+            PreviewComponent={PreviewComponent}
+            isVisible={activeTabIndex === "preview" ? false : true}
+          />
+        )}
 
-        {activeTabIndex === "code" && <CopyToClipBoard textToCopy={CodeSnippets} />}
+        {activeTabIndex === "code" && (
+          <CopyToClipBoard textToCopy={CodeSnippets} />
+        )}
         <section
           className={`${
             activeTabIndex === "code" ? "block" : "hidden"
-          } w-auto max-h-[520px] overflow-scroll box-border scroll-smooth md:m-4 m-2 bg-gradient-to-tr from-Nav to-Nav/10 relative`}
+          } relative m-2   box-border max-h-[520px] w-auto overflow-scroll scroll-smooth bg-gradient-to-tr from-Nav to-Nav/10 md:m-4`}
         >
           <SyntaxHighlighter CodeSnippets={CodeSnippets} Language={"jsx"} />
         </section>
