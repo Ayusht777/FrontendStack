d1e4b704f9328287db349ae1120ea4683eaa71a8
Ayusht777 <talesarayush@gmail.com>
2024-04-27 19:18:31 +0530

    feat: Integrate dynamic user data into PreviewPage.jsx

--- src/Pages/PreviewPage.jsx
+++ src/Pages/PreviewPage.jsx
@@ -1,25 +1,31 @@
 import { componentsData } from "@/Data/Data";
 import HeroSection from "@/Components/PreviewPageComponents/HeroSection";
-import Test from "@/Components/Test";
+import Navbar from "@/Components/Shared/Navbar";
 import CodeBlock from "@/Components/PreviewPageComponents/CodeBlockComponent/CodeBlock";
 import StepsSection from "@/Components/PreviewPageComponents/StepsComponent/StepsSection";
 import ReferenceSection from "@/Components/PreviewPageComponents/ReferenceSection";
 import ToolbarSection from "@/Components/PreviewPageComponents/ToolbarSection";
 import { useParams } from "react-router-dom";
-const PreviewPage = () => {
-  const p = useParams();
-  console.log(p)
-  
-  return (
-    <div className="w-full h-full flex justify-center items-center flex-col bg-Bg  md:px-0 px-4">
-      <Test />
-      <HeroSection Title={"Explore the Live Preview and Visualize the Code"}/>
-      <ToolbarSection />
-      <CodeBlock
-        CodeSnippets={componentsData[0].code}
-        PreviewComponent={componentsData[0].renderedComponent}
-      />
+import { useState } from "react";
 
+const PreviewPage = () => {
+  const componentName = useParams();
+  const [SelectedAuthor, setSelectedAuthor] = useState("");
+
+  const filteredVariation = componentsData
+    .find((item) => item.name === componentName.ComponentName)
+    .variations.find((variation) => variation.author === SelectedAuthor);
+  console.log(filteredVariation);
+
+  return (
+    <div className="flex h-full w-full flex-col items-center justify-center bg-Bg px-4 md:px-0">
+      <Navbar />
+      <HeroSection Title={"Explore the Live Preview and Visualize the Code"} />
+      <ToolbarSection getSelectedAuthor={setSelectedAuthor} />
+      <CodeBlock
+        CodeSnippets={filteredVariation?.code}
+        PreviewComponent={filteredVariation?.renderedComponent}
+      />
       <StepsSection />
       <ReferenceSection />
     </div>
