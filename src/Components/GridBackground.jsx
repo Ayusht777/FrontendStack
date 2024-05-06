6739e8e8d07d367610087aefa03ddea6d92a60ec
Ayusht777 <talesarayush@gmail.com>
2024-05-06 21:11:44 +0530

    feat(GridBackground.jsx): Add aspect ratio to grid background component

--- src/Components/GridBackground.jsx
+++ src/Components/GridBackground.jsx
@@ -1,6 +1,6 @@
 const GridBackground = ({ section }) => {
   return (
-    <div className=" relative  box-border size-full  overflow-hidden rounded-3xl outline outline-1  outline-Border">
+    <div className=" relative  box-border size-full  overflow-hidden rounded-3xl outline outline-1  outline-Border aspect-square">
       <div className="absolute inset-0 bg-Nav bg-[linear-gradient(to_right,#404040_1px,transparent_1px),linear-gradient(to_bottom,#404040_1px,transparent_1px)] bg-[size:48px_48px] "></div>
       {section}
     </div>
