07cdd2d012d3ea117a3c531fc642c7c3890a8808
Ayusht777 <talesarayush@gmail.com>
2024-05-06 18:00:42 +0530

    fix(GridBackground.jsx): Remove unnecessary console log

--- src/Components/GridBackground.jsx
+++ src/Components/GridBackground.jsx
@@ -1,5 +1,4 @@
 const GridBackground = ({ section }) => {
-  console.log(section);
   return (
     <div className=" relative  box-border size-full  overflow-hidden rounded-3xl outline outline-1  outline-Border">
       <div className="absolute inset-0 bg-Nav bg-[linear-gradient(to_right,#404040_1px,transparent_1px),linear-gradient(to_bottom,#404040_1px,transparent_1px)] bg-[size:48px_48px] "></div>
