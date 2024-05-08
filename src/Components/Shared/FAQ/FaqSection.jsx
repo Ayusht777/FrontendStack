194f3b48cb20870abfc76c87a78cb7dde7ae2b7f
Ayusht777 <talesarayush@gmail.com>
2024-05-08 12:37:26 +0530

    refactor: Update FaqSection to use index as key in faqData map

--- src/Components/Shared/FAQ/FaqSection.jsx
+++ src/Components/Shared/FAQ/FaqSection.jsx
@@ -1,13 +1,14 @@
 import { useState } from "react";
 import { ChevronDown } from "lucide-react";
-import {faqData} from "@/Components/Shared/FAQ/faqData";
+import { faqData } from "@/Components/Shared/FAQ/faqData";
+import Button from "@/Components/Shared/Button";
 import Heading from "@/Components/Shared/Heading";
 const FaqItems = ({ question, answer }) => {
   const [IsOpen, setIsOpen] = useState(false);
   return (
-    <div className="my-6">
+    <div className="my-4 w-full">
       <button
-        className={`flex w-full items-center justify-between  bg-Nav px-2 py-3 outline outline-1 outline-Border  ${IsOpen ? "rounded-t-lg" : "rounded-lg"}`}
+        className={`flex w-full items-center justify-between  bg-Nav px-2 py-2.5 outline outline-1 outline-Border  ${IsOpen ? "rounded-t-lg" : "rounded-lg"}`}
         onClick={() => setIsOpen(!IsOpen)}
       >
         <p className="w-full text-start text-sm font-semibold tracking-tight text-white md:text-base md:tracking-normal">
@@ -28,11 +29,17 @@ const FaqItems = ({ question, answer }) => {
 
 const FaqSection = () => {
   return (
-    <section className="w-full max-w-[1200px] ">
-         <Heading title={"FAQ'S"}/>{
-       
-        faqData.map((item,index)=><FaqItems key={index} question={item.question} answer={item.answer} />)
-    }</section>
+    <section className="flex w-full max-w-[1200px] flex-col items-center py-24">
+      <Heading title={"FAQ'S"} />
+      {faqData.map((item, index) => (
+        <FaqItems key={index} question={item.question} answer={item.answer} />
+      ))}
+      <Button
+        title={"View More"}
+        background_color={"Border"}
+        text_color={"white"}
+      />
+    </section>
   );
 };
 
