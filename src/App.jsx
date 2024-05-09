d7e04afb48e5e698d9eec3bd6a3e5ddbeb38169d
Ayusht777 <talesarayush@gmail.com>
2024-05-09 20:46:51 +0530

    refactor: Add QueryForm component for improved user experience

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import AppRoutes from "@/Routes/AppRoutes";
-
+import QueryForm from "@/Components/HomeComponents/QueryForm/QueryForm";
 
 function App() {
   return (
     <>
-      <AppRoutes></AppRoutes>
-      
+      {/* <AppRoutes></AppRoutes> */}
+      <section className="max-w-[1200px] w-full bg-Bg  h-screen mx-auto text-white">
+        <QueryForm></QueryForm>
+       
+      </section>
     </>
   );
 }
