399b9670ae90630af8288fc85ed0a3eb66f1e005
Ayusht777 <talesarayush@gmail.com>
2024-05-09 21:59:19 +0530

    refactor: Update QueryForm component with improved styling and form validation

--- src/Components/HomeComponents/QueryForm/QueryForm.jsx
+++ src/Components/HomeComponents/QueryForm/QueryForm.jsx
@@ -1,5 +1,6 @@
 import { useForm } from "react-hook-form";
 import DropDownMenu from "@/Components/DropDownMenu";
+import Heading from '@/Components/Shared/Heading'
 const QueryForm = () => {
   const { register, handleSubmit } = useForm();
   const onSubmit = (data) => {
@@ -7,35 +8,41 @@ const QueryForm = () => {
   };
 
   return (
-    <form
-      className=" flex max-w-64 flex-col items-start"
-      onSubmit={handleSubmit(onSubmit)}
-    >
-      <label>Full Name</label>
-      <input
-        type="text"
-        {...register("FullName")}
-        className="mb-1 mt-2 box-border w-full rounded-md bg-inherit px-3 py-1 outline outline-1  outline-Border focus:outline-Logo"
-        placeholder="Ayush"
-      />
-      <label>Email</label>
-      <input
-        type="email"
-        {...register("Email")}
-        className="mb-1 mt-2 box-border w-full rounded-md bg-inherit px-3 py-1 outline outline-1  outline-Border focus:outline-Logo"
-        placeholder="ayush@mail.com"
-      />
-      <label>Subject</label>
-      <DropDownMenu />
-      <label>Message</label>
-      <textarea
-        {...register("Message")}
-        placeholder="Type your massage here."
-        rows={4}
-        className="mb-1 mt-2 box-border w-full rounded-md  bg-inherit px-3 py-1 outline outline-1 outline-Border focus:outline-Logo"
-      ></textarea>
-      <input type="submit" />
-    </form>
+    <div className="mx-auto max-w-lg py-4 text-white">
+      <Heading title={"Connect with the Team"} />
+      <p className=" -mt-6 mb-4 pl-4 ">
+        Do you have any suggestions for this project or have any issues with it?
+      </p>
+      <form
+        className="  mx-auto flex  flex-col items-start px-4"
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <label className="my-2">Full Name</label>
+        <input
+          type="text"
+          {...register("FullName")}
+          className="mb-1.5 box-border w-full rounded-md bg-Bg px-3 py-1 outline outline-1 outline-Border  focus:bg-Nav focus:outline-Logo"
+          placeholder="Ayush Talesara"
+        />
+        <label className="my-2">Email</label>
+        <input
+          type="email"
+          {...register("Email")}
+          className="mb-1.5 box-border w-full rounded-md bg-Bg px-3 py-1 outline outline-1 outline-Border  focus:bg-Nav focus:outline-Logo"
+          placeholder="ayush@mail.com"
+        />
+        <label className="my-2">Subject</label>
+        <DropDownMenu />
+        <label className="my-2">Message</label>
+        <textarea
+          {...register("Message")}
+          placeholder="Type your massage here."
+          rows={4}
+          className="mb-1.5 box-border w-full rounded-md  bg-Bg px-3 py-1 outline outline-1 outline-Border focus:bg-Nav focus:outline-Logo"
+        ></textarea>
+        <input type="submit" className="my-2" />
+      </form>
+    </div>
   );
 };
 
---
+++ src/Components/HomeComponents/QueryForm/QueryFormSection.jsx
@@ -0,0 +1,11 @@
+import QueryForm from "@/Components/HomeComponents/QueryForm/QueryForm";
+
+const QueryFormSection = () => {
+  return (
+    <section className="">
+      <QueryForm />
+    </section>
+  );
+};
+
+export default QueryFormSection;
