d3d6dc35c83642df70e06a07aa9fc4cf6a7b8ad4
Ayusht777 <talesarayush@gmail.com>
2024-05-10 21:11:15 +0530

    refactor: Update Button component with improved class names and text color handling

--- src/Components/Shared/Button.jsx
+++ src/Components/Shared/Button.jsx
@@ -1,7 +1,7 @@
 const Button = ({ title, background_color = "Logo", text_color = "Bg" }) => {
   return (
     <button
-      className={` my-4 min-w-16 rounded-lg   px-5 py-1.5 text-center font-semibold tracking-tight  transition-colors duration-300 ease-in-out focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2  ${`bg-` + background_color} ${`text-` + text_color} ${`hover:bg-` + background_color + `/90`} ${`focus:outline-` + background_color} `}
+      className={` my-4 min-w-16 rounded-lg   px-5 py-1.5 text-center font-semibold tracking-tight  transition-colors duration-300 ease-in-out focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2  ${`bg-` + background_color} text-${text_color} ${`hover:bg-` + background_color + `/90`} ${`focus:outline-` + background_color} `}
     >
       {title}
     </button>
