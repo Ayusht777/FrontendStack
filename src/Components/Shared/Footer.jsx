9543ef84705117dde030cb345f70dc972dd40d89
Ayusht777 <talesarayush@gmail.com>
2024-05-11 09:04:46 +0530

    refactor: Update Footer component with improved styling and accessibility

--- src/Components/Shared/Footer.jsx
+++ src/Components/Shared/Footer.jsx
@@ -1,32 +1,22 @@
-import { HiMiniXMark } from "react-icons/hi2";
-import { Link } from "react-router-dom";
-function Footer({ className }) {
+import { Plus } from "lucide-react";
+const Footer = () => {
   return (
-    <footer
-      className={`border-2 border-b-0 border-Border rounded-t-lg w-4/5 md:p-3 p-1 flex flex-col items-center justify-center md:space-y-5 space-y-2 cursor-pointer ${className}`}
-    >
-      <Link
-        to="/"
-        className="text-Logo md:text-4xl text-2xl font-bold !mb-5 !mt-3"
-      >
-        StackFrontend
-      </Link>
-      <h2 className="text-white md:text-xl   font-bold text-center">
-        Showcasing Our FrontendComponents For You
-      </h2>
-      <div className="flex gap-2 items-center justify-center ">
-        <hr className="md:w-52 w-24 rounded-sm" />
-        <HiMiniXMark className="text-Logo text-4xl" />
-        <hr className="md:w-52 w-24 rounded-sm" />
-      </div>
-      <p className="text-white md:text-base text-sm">
-        copyright ©2024
-        <Link to="/" className="text-Logo pl-1">
+    <footer className="w-full max-w-[1200px] ">
+      <div className=" flex flex-col items-center justify-center gap-y-2 rounded-t-lg py-4 outline outline-2 outline-Nav">
+        <h1 className="text-2xl font-semibold text-Logo md:text-3xl lg:text-4xl">
           StackFrontend
-        </Link>
-      </p>
+        </h1>
+        <div className="flex  items-center justify-center">
+          <hr className="w-[4.5rem] border-[1px]  md:w-24 lg:w-28 " />
+          <Plus className="rotate-45 text-Logo " />
+          <hr className=" w-[4.5rem] border-[1px]  md:w-24 lg:w-28" />
+        </div>
+        <p className='text-sm text-white after:pl-1 after:text-Logo after:content-["StackFrontend"] lg:text-base'>
+          copyright ©2024
+        </p>
+      </div>
     </footer>
   );
-}
+};
 
 export default Footer;
