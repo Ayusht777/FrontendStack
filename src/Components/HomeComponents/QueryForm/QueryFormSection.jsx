60965cfc1fd4f6461384e998e713dcb78de79d29
Ayusht777 <talesarayush@gmail.com>
2024-05-11 10:30:57 +0530

    refactor: Update QueryFormSection component with improved styling and responsiveness

--- src/Components/HomeComponents/QueryForm/QueryFormSection.jsx
+++ src/Components/HomeComponents/QueryForm/QueryFormSection.jsx
@@ -2,9 +2,11 @@ import QueryForm from "@/Components/HomeComponents/QueryForm/QueryForm";
 
 const QueryFormSection = () => {
   return (
-    <section className="py-16 ">
-      <div className="mx-auto max-w-screen-xl rounded-lg p-4 text-white outline outline-1 outline-Border ">
-        <QueryForm />
+    <section className=" relative  my-16 w-full max-w-[1200px] ">
+      <div className="   w-full rounded-lg bg-[radial-gradient(#D7F400_1px,#111213_1px)]  p-0 text-white   outline outline-2 outline-Border md:bg-[size:1.5rem_1.5rem] md:py-16">
+        <div className=" mx-auto max-w-xl rounded-lg bg-Bg  md:p-4 p-2  outline outline-2 outline-Logo">
+          <QueryForm />
+        </div>
       </div>
     </section>
   );
