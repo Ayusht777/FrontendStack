dadc9c4fad9fa06e98282d1ad1530735060f0345
Ayusht777 <talesarayush@gmail.com>
2024-05-15 11:30:02 +0530

    feat: Add author selection dropdown in PreviewPage
    Add author selection dropdown in PreviewPage for users to choose the component variation author

--- src/Pages/PreviewPage.jsx
+++ src/Pages/PreviewPage.jsx
@@ -1,4 +1,4 @@
-import { componentsData } from "@/Data/Data";
+import { componentsData, authors } from "@/Data/Data";
 import HeroSection from "@/Components/Shared/HeroSection";
 import Navbar from "@/Components/Shared/Navbar";
 import CodeBlock from "@/Components/PreviewPageComponents/CodeBlockComponent/CodeBlock";
@@ -10,18 +10,26 @@ import { useState } from "react";
 
 const PreviewPage = () => {
   const componentName = useParams();
-  const [SelectedAuthor, setSelectedAuthor] = useState("");
-
+  const [SelectedAuthor, setSelectedAuthor] = useState(authors[0].author);
   const filteredVariation = componentsData
     .find((item) => item.name === componentName.ComponentName)
     .variations.find((variation) => variation.author === SelectedAuthor);
   console.log(filteredVariation);
+  const authorList = authors.map((author) => author.author);
+  const imglist = authors.map((author) => author.img);
+  console.log(imglist)
 
   return (
     <div className="flex h-full w-full flex-col items-center justify-center bg-Bg px-4 md:px-6 xl:px-0">
       <Navbar />
-      <HeroSection  heading={"Explore the Live Preview and Visualize the Code"} />
-      <ToolbarSection getSelectedAuthor={setSelectedAuthor} />
+      <HeroSection
+        heading={"Explore the Live Preview and Visualize the Code"}
+      />
+      <ToolbarSection
+        onOptionChange={setSelectedAuthor}
+        dropdownOptions={authorList}
+        img={imglist}
+      />
       <CodeBlock
         CodeSnippets={filteredVariation?.code}
         PreviewComponent={filteredVariation?.renderedComponent}
