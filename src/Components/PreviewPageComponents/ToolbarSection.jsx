ed4c6a67e0210862f65bb682892280c55e584d1b
Ayusht777 <talesarayush@gmail.com>
2024-05-15 11:27:16 +0530

    refactor: Update ToolbarSection component
    Refactor ToolbarSection component to use a shared DropdownMenu component and remove duplicate imports

--- src/Components/PreviewPageComponents/ToolbarSection.jsx
+++ src/Components/PreviewPageComponents/ToolbarSection.jsx
@@ -1,16 +1,22 @@
 import { ArrowLeft } from "lucide-react";
 import { Link } from "react-router-dom";
-import DropdownMenu from "@/Components/PreviewPageComponents/DropdownMenuComponent/DropdownMenu";
+import DropdownMenu from "@/Components/Shared/DropDownMenu";
 
-const ToolbarSection = ({ getSelectedAuthor }) => {
+const ToolbarSection = ({ onOptionChange, dropdownOptions,img }) => {
+  console.log(img)
   return (
-    <section className="my-4 flex w-full max-w-[1200px] items-start justify-between">
+    <section className="relative z-10 my-4 flex w-full max-w-[1200px] items-start justify-between text-white">
       <button className="size-7 rounded-md bg-Nav text-Logo ">
         <Link to="/gallery">
           <ArrowLeft strokeWidth="2" className="mx-auto " />
         </Link>
       </button>
-      <DropdownMenu getSelectedAuthor={getSelectedAuthor} />
+      <DropdownMenu
+        dropdownOptions={dropdownOptions}
+        onOptionChange={onOptionChange}
+        form={false}
+        selectedOptionImage={img}
+      />
     </section>
   );
 };
