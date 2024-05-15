1bb8b5a0cb251bf0ff33eced3e9c0853d5aba568
Ayusht777 <talesarayush@gmail.com>
2024-05-15 21:53:01 +0530

    style: Increase size of button in ToolbarSection component
    Updated the size of the button in the ToolbarSection component:
    - Increased the size from 'size-7' to 'size-8' to improve visibility and touchability.
    
    This style enhancement enhances the user experience of navigating back to the gallery.

--- src/Components/PreviewPageComponents/ToolbarSection.jsx
+++ src/Components/PreviewPageComponents/ToolbarSection.jsx
@@ -5,7 +5,7 @@ import DropdownMenu from "@/Components/Shared/DropDownMenu";
 const ToolbarSection = ({ dropdownOptions,selectedAuthorName,onOptionChange }) => {
   return (
     <section className="relative z-10 my-4 flex w-full max-w-[1200px] items-start justify-between text-white">
-      <button className="size-7 rounded-md bg-Nav text-Logo ">
+      <button className="size-8 rounded-md bg-Nav text-Logo ">
         <Link to="/gallery">
           <ArrowLeft strokeWidth="2" className="mx-auto " />
         </Link>
