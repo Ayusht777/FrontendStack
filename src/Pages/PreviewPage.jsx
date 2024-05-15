2b07a67b92233af0fc22406fb0c2a5b73e5a3623
Ayusht777 <talesarayush@gmail.com>
2024-05-15 19:05:18 +0530

    feat: Implement redirection to 404 page when no component variation is found
    Added logic to handle the scenario where no component variation is found for the selected author. If no variation is found, the component redirects to the 404_NOT_FOUND page.

--- src/Pages/PreviewPage.jsx
+++ src/Pages/PreviewPage.jsx
@@ -5,19 +5,24 @@ import CodeBlock from "@/Components/PreviewPageComponents/CodeBlockComponent/Cod
 import StepsSection from "@/Components/PreviewPageComponents/StepsComponent/StepsSection";
 import ReferenceSection from "@/Components/PreviewPageComponents/ReferenceSection";
 import ToolbarSection from "@/Components/PreviewPageComponents/ToolbarSection";
-import { useParams } from "react-router-dom";
+import { useParams, Navigate } from "react-router-dom";
 import { useState } from "react";
-
 const PreviewPage = () => {
+  const randomAuthorIndex = Math.floor(Math.random() * authors.length);
   const componentName = useParams();
-  const [SelectedAuthor, setSelectedAuthor] = useState(authors[0].author);
-  const filteredVariation = componentsData
-    .find((item) => item.name === componentName.ComponentName)
-    .variations.find((variation) => variation.author === SelectedAuthor);
-  console.log(filteredVariation);
-  const authorList = authors.map((author) => author.author);
-  const imglist = authors.map((author) => author.img);
-  console.log(imglist)
+  const [selectedAuthorName, setselectedAuthorName] = useState(
+    authors[randomAuthorIndex].author
+  );
+  const filteredVariation =
+    componentsData
+      ?.find((item) => item.name === componentName.ComponentName)
+      ?.variations.find(
+        (variation) => variation.author === selectedAuthorName
+      ) || false;
+
+  if (!filteredVariation) {
+    return <Navigate to={"/404_NOT_FOUND"} replace={true} />;
+  }
 
   return (
     <div className="flex h-full w-full flex-col items-center justify-center bg-Bg px-4 md:px-6 xl:px-0">
@@ -26,9 +31,9 @@ const PreviewPage = () => {
         heading={"Explore the Live Preview and Visualize the Code"}
       />
       <ToolbarSection
-        onOptionChange={setSelectedAuthor}
-        dropdownOptions={authorList}
-        img={imglist}
+        dropdownOptions={authors}
+        selectedAuthorName={selectedAuthorName}
+        onOptionChange={setelectedAuthorName}
       />
       <CodeBlock
         CodeSnippets={filteredVariation?.code}
