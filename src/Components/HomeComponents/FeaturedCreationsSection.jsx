b0c3c87062f14e3134fc9919e712828cf69faa0a
Ayusht777 <talesarayush@gmail.com>
2024-05-15 11:25:58 +0530

    refactor: Update FeaturedCreationsSection component
    Refactor FeaturedCreationsSection component to use state for RecentCreation and implement responsive behavior

--- src/Components/HomeComponents/FeaturedCreationsSection.jsx
+++ src/Components/HomeComponents/FeaturedCreationsSection.jsx
@@ -1,19 +1,36 @@
+import { useEffect, useState } from "react";
 import Data from "@/Data/GalleryData";
 import GalleryGrids from "@/Components/GalleryComponents/GalleryGrids";
 import Heading from "@/Components/Shared/Heading";
 import Button from "@/Components/Shared/Button";
+
 const FeaturedCreationsSection = () => {
-  const RecentCreation = Data.length > 3 ? Data.slice(-3).reverse() : null;
+  const [RecentCreation, setRecentCreation] = useState(Data.slice(-3).reverse());
+
+ useEffect(() => {
+   const handleResize = () => {
+     const sliceLength = window.innerWidth === 834 ? 4 : 3;
+     setRecentCreation(Data.slice(-sliceLength).reverse());
+   };
+   window.addEventListener("resize", handleResize);
+   return () => window.removeEventListener("resize", handleResize);
+ }, []);
 
   return (
     <section className="w-full max-w-[1200px]  md:pb-24 md:pt-0">
       <Heading title={"Top Creations"} />
       <GalleryGrids Data={RecentCreation} />
-      <div className="flex justify-center my-8">
-        <Button title={"View More"} background_color="Border" text_color="white" />
+      <div className="my-8 flex justify-center">
+        <Button
+          title={"View More"}
+          background_color="Border"
+          text_color="white"
+          path={"/gallery"}
+        />
       </div>
     </section>
   );
 };
 
 export default FeaturedCreationsSection;
+
