2310e143008bb82e42279342e15bc651f36b3ba7
Ayusht777 <talesarayush@gmail.com>
2024-05-15 09:45:50 +0530

    Refactor Button components, introduce react-router navigation and update link functionality.

--- src/Components/Shared/Button.jsx
+++ src/Components/Shared/Button.jsx
@@ -1,9 +1,10 @@
-const Button = ({ title, background_color = "Logo", text_color = "Bg" }) => {
+import {Link} from 'react-router-dom'
+const Button = ({ title, background_color = "Logo", text_color = "Bg" ,path}) => {
   return (
     <button
-      className={` my-4 min-w-16 rounded-lg   px-5 py-1.5 text-center font-semibold tracking-tight  transition-colors duration-300 ease-in-out focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2  ${`bg-` + background_color} text-${text_color} ${`hover:bg-` + background_color + `/90`} ${`focus:outline-` + background_color} `}
+      className={` min-w-16 rounded-lg   px-5 py-1.5 text-center font-semibold tracking-tight  transition-colors duration-300 ease-in-out focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2  ${`bg-` + background_color} text-${text_color} ${`hover:bg-` + background_color + `/90`} ${`focus:outline-` + background_color} `}
     >
-      {title}
+      <Link to={path}>{title}</Link>
     </button>
   );
 };
--- src/Components/Shared/ButtonV2.jsx
+++ src/Components/Shared/ButtonV2.jsx
@@ -1,16 +1,20 @@
 import { Github } from "lucide-react";
 const ButtonV2 = () => {
   return (
-    <button
-      className={` box-border  flex  min-w-16 items-center  justify-between 
+    <button>
+      <a
+        href="https://github.com/Ayusht777/FrontendStack"
+        target="_blank"
+        className={` box-border  flex  min-w-16 items-center  justify-between 
       rounded-lg bg-inherit px-5 py-1.5 text-center 
       font-semibold tracking-tight text-white 
       outline outline-1 outline-Border transition-colors 
       duration-300 ease-in-out hover:bg-Border focus:outline-white  
       focus-visible:outline focus-visible:outline-1
       focus-visible:outline-offset-2`}
-    >
-      <Github className="mr-1.5 size-4 rounded-full " /> Github
+      >
+        <Github className="mr-1.5 size-4 rounded-full " /> Github
+      </a>
     </button>
   );
 };
