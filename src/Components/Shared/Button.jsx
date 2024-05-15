d57c88e92e2433dd56878386e8b20b7f4d59ea67
Ayusht777 <talesarayush@gmail.com>
2024-05-15 19:05:47 +0530

    feat: Implement conditional rendering for Button component
    Added conditional rendering to the Button component to allow it to behave as a link when a 'path' prop is provided. If no 'path' prop is provided, the component renders a standard button.

--- src/Components/Shared/Button.jsx
+++ src/Components/Shared/Button.jsx
@@ -1,11 +1,21 @@
 import {Link} from 'react-router-dom'
-const Button = ({ title, background_color = "Logo", text_color = "Bg" ,path}) => {
-  return (
-    <button
-      className={` min-w-16 rounded-lg   px-5 py-1.5 text-center font-semibold tracking-tight  transition-colors duration-300 ease-in-out focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2  ${`bg-` + background_color} text-${text_color} ${`hover:bg-` + background_color + `/90`} ${`focus:outline-` + background_color} `}
-    >
-      <Link to={path}>{title}</Link>
-    </button>
-  );
+const Button = ({
+  title,
+  background_color = "Logo",
+  text_color = "Bg",
+  path,
+}) => {
+  const buttonClasses = `min-w-16 rounded-lg px-5 py-1.5 text-center font-semibold tracking-tight transition-colors duration-300 ease-in-out focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 ${`bg-` + background_color} text-${text_color} ${`hover:bg-` + background_color + `/90`} ${`focus:outline-` + background_color}`;
+
+  if (path) {
+    return (
+      <Link to={path}>
+        <button className={buttonClasses}>{title}</button>
+      </Link>
+    );
+  }
+
+  return <button className={buttonClasses}>{title}</button>;
 };
+
 export default Button;
