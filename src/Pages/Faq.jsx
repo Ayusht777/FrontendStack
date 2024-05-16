c2490922886f212898ddb50db5c40302a68b5f2c
Ayusht777 <talesarayush@gmail.com>
2024-05-16 21:01:47 +0530

    feat: Implement FAQ page with heading, description, and dynamic FAQ items (v1 -> v2) * Replace placeholder content with FAQ section including heading, description, and space for FAQs. * Implement dynamic rendering of FAQ items from data using `faqData` and `FaqItems` component. * Add `Footer` component to the page layout.

--- src/Pages/Faq.jsx
+++ src/Pages/Faq.jsx
@@ -1,10 +1,34 @@
 import Navbar from "@/Components/Shared/Navbar";
-import FaqList from "@/Components/Shared/FAQ/FaqSection";
+import { faqData } from "@/Components/Shared/FAQ/faqData";
+import FaqItems from "@/Components/Shared/FAQ/FaqItems";
+import Button from "@/Components/Shared/Button";
+import Footer from "@/Components/Shared/Footer";
 const Faq = () => {
   return (
-    <div className="flex h-screen w-full flex-col items-center justify-start bg-Bg">
+    <div className="flex h-full min-h-screen w-full flex-col items-center justify-start bg-Bg">
       <Navbar></Navbar>
-      <FaqList />
+      <section className=" my-4 flex  w-full flex-col items-center justify-center py-20">
+        <span className="max-w-5xl   bg-gradient-to-r from-white to-white/70 bg-clip-text text-center text-3xl font-semibold capitalize tracking-tighter text-transparent md:py-1.5 md:text-5xl lg:text-7xl">
+          Frequently Asked Questions
+        </span>
+
+        <p className="my-4  max-w-4xl text-center text-lg font-light tracking-tight text-white/90  decoration-Border underline-offset-4 md:text-xl">
+          Discover how to tackle this design components challenge with ease. If
+          you have any specific questions or need further clarification, our
+          team is happy to assist.
+        </p>
+      </section>
+      <section className="flex w-full max-w-[1200px] flex-col items-center pb-32 pt-4">
+        {faqData.map((item, index) => (
+          <FaqItems
+            key={index}
+            question={item.question}
+            answer={item.answer}
+            open={item?.open}
+          />
+        ))}
+      </section>
+      <Footer />
     </div>
   );
 };
