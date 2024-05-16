c368723d675aa26fcfb2fce1b5117aaf456c3dd5
Ayusht777 <talesarayush@gmail.com>
2024-05-16 21:00:14 +0530

    docs: Update FAQ content with detailed information (v1 -> v2)

--- src/Components/Shared/FAQ/faqData.js
+++ src/Components/Shared/FAQ/faqData.js
@@ -1,28 +1,53 @@
 export const faqData = [
   {
     question: "What is the purpose of this challenge?",
-    answer:
-      "This challenge aims to enhance our skills in React.js by creating 30 different components for coding practice and displaying them on a website",
+    answer: `This challenge is designed to help us enhance our frontend development abilities by building reusable UI components using React.js and Tailwind CSS. 
+      It's similar to challenges like 100 Days of Code, but specifically focused on component creation. 
+      The website will showcase our components with live previews and code snippets, allowing you to see the implementation in action. 
+      While there are no external prizes, the primary benefit is the significant improvement in our React.js and frontend development skills through practical experience with UI components.`,
+    open: true,
   },
   {
-    question: "How can I participate in this challenge?",
-    answer:
-      "This challenge aims to enhance our skills in React.js by creating 30 different components for coding practice and displaying them on a website",
-  },
-  {
-    question: "Are there any prizes for completing the challenge?",
-    answer:
-      "This challenge aims to enhance our skills in React.js by creating 30 different components for coding practice and displaying them on a website",
+    question: "Where will you be sharing the components you built?",
+    answer: `We will share the components on this website, on GitHub, and in a post on LinkedIn.`,
   },
   {
     question: "How will the components be displayed on the website?",
-    answer:
-      "This challenge aims to enhance our skills in React.js by creating 30 different components for coding practice and displaying them on a website",
+    answer: `Each component will be showcased on the website with a live preview demonstrating its functionality.
+       Additionally, we'll include the corresponding code snippet for each component, allowing users to see the implementation behind the scenes. 
+       This provides a clear understanding of how the components work and how to use them in their own projects.`,
   },
   {
     question:
-      "Can I use libraries or frameworks other than React.js  challenge?",
-    answer:
-      "This challenge aims to enhance our skills in React.js by creating 30 different components for coding practice and displaying them on a website",
+      "Are there any specific design guidelines or best practices we are following for building the components?",
+    answer: `Yes, we have specific design principles and accessibility guidelines. 
+      You can refer to these resources for inspiration: [shadcn/ui], NextUI, GreatFrontEnd.`,
+  },
+  {
+    question: "What are the long-term plans for this project?",
+    answer: `We plan to enhance this project in several ways:
+            adding TypeScript Support, implementing Component Animations, and more. Stay tuned! You can follow our progress and see future plans for the project on GitHub.`,
+  },
+  {
+    question:
+      "Is there a plan to document the learnings and outcomes of the challenge after it's completed?",
+    answer: `Yes, we absolutely plan to document the learnings and outcomes of the challenge! 
+    We believe it's valuable to share our experiences and insights to benefit ourselves and others.`,
+  },
+  {
+    question: "How can we participate in the challenge?",
+    answer: `As of now, this is a private project where we showcase our creations. 
+      However, we plan to open participation to more users in the future. Stay tuned!`,
+  },
+  {
+    question: "Are there any prizes for completing the challenge?",
+    answer: `No, there are no external prizes for completing the challenge. 
+      However, the primary benefit is the significant improvement in our frontend development skills through practical experience building reusable components.
+       This can make us more competitive in job interviews and enhance our ability to tackle real-world frontend development projects.`,
+  },
+  {
+    question: "What is the timeframe for completing the challenge?",
+    answer: `There's no specific target duration. We encourage a flexible approach, depending on our individual goals. 
+      Whether you aim to build a few components quickly or take a more in-depth exploration, the challenge is designed to fit our individual learning pace.`,
   },
 ];
