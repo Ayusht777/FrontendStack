2876759796bdf133261e54905cd5d1f1476afe1e
Ayusht777 <talesarayush@gmail.com>
2024-05-16 21:02:35 +0530

    feat: Implement GridBackgroundV2 for Home page with content layering (v1 -> v2) * Introduce GridBackgroundV2 component to enhance the visual style of the Home page. * Wrap existing content within a positioned div to ensure proper layering over the background.

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,26 +1,28 @@
 import Navbar from "@/Components/Shared/Navbar";
 import Footer from "@/Components/Shared/Footer";
-import FaqList from "@/Components/Shared/FAQ/FaqSection";
+import FaqSection from "@/Components/HomeComponents/FaqSection";
 import QueryFormSection from "@/Components/HomeComponents/QueryForm/QueryFormSection";
 import FeatureContainer from "@/Components/HomeComponents/FeatureSection/FeatureContainer";
 import HeroSection from "@/Components/Shared/HeroSection";
 import FeaturedCreationsSection from "@/Components/HomeComponents/FeaturedCreationsSection";
-
+import GridBackgroundV2 from "@/Components/Shared/GridBackgroundV2";
 const Home = () => {
   return (
-    <div className="flex h-full w-full flex-col items-center justify-center bg-Bg px-4 md:px-6 xl:px-0">
-      <Navbar />
-      <HeroSection
-        heading={"Exploring Our User Interface Building Blocks"}
-        subheading={"Dive Deep Exploring Core UI Components"}
-        callToAction={true}
-      />
-      <FeaturedCreationsSection />
-      <FeatureContainer />
-      <FaqList></FaqList>
-      <QueryFormSection></ QueryFormSection>
-      <Footer></Footer>
-    </div>
+    <GridBackgroundV2>
+      <div className=" relative z-20 flex h-full w-full flex-col items-center justify-center px-4 md:px-6 xl:px-0">
+        <Navbar />
+        <HeroSection
+          heading={"Exploring Our User Interface Building Blocks"}
+          subheading={"Dive Deep Exploring Core UI Components"}
+          callToAction={true}
+        />
+        <FeaturedCreationsSection />
+        <FeatureContainer />
+        <FaqSection />
+        <QueryFormSection></QueryFormSection>
+        <Footer></Footer>
+      </div>
+    </GridBackgroundV2>
   );
 };
 
