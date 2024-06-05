74b8707e7a90bb8408cf47c8406e730729887a16
Ayusht777 <talesarayush@gmail.com>
2024-06-05 19:52:20 +0530

    fix: Adjust size and styling of 'Component Not Found' icon
    - Updates the size of the 'PackageX' icon (presumably representing 'component not found') in the 'ComponentNotFound404' component from 'size-64' to 'md:size-64 size-48'.
    - Adjusts the maximum width of the error message on medium screens (md) from 'max-w-xl' to 'md:max-w-xl'.
    
    These changes address potential layout issues and ensure consistency in the component's appearance.

--- src/Pages/ComponentNotFound404.jsx
+++ src/Pages/ComponentNotFound404.jsx
@@ -12,11 +12,11 @@ const ComponentNotFound404 = () => {
         aria-label="Page Not Found "
       >
         <div className=" flex  w-4/5 flex-col items-center justify-center">
-          <div className="mb-8 box-border flex size-64 items-center justify-center rounded-full bg-Nav p-8 ">
+          <div className="mb-8 box-border flex md:size-64 size-48 items-center justify-center rounded-full bg-Nav p-8 ">
             <PackageX className=" size-full text-Logo" />
           </div>
           <div className="flex flex-col text-center leading-10 md:-mt-4 ">
-            <h2 className=" text-2xl text-white md:text-3xl max-w-xl">
+            <h2 className=" text-2xl text-white md:text-3xl md:max-w-xl">
               We're sorry, the component you're looking for hasn't been uploaded
               yet.
             </h2>
