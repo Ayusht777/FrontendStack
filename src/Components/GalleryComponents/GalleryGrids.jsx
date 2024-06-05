582467e56b1a5c7d02ccca198321dd7c78a18dea
Ayusht777 <talesarayush@gmail.com>
2024-06-05 19:53:03 +0530

    fix: Handle cases where 'isUploaded' data is missing
    - Updates the conditional logic within the button to check for the existence of the `Data.isUploaded` property before constructing the link.
    - If `Data.isUploaded` is missing or false, the button now links to the 'ComponentNotFound404' route, preventing potential errors.
    
    This commit ensures the gallery grid handles scenarios where the 'isUploaded' data is not available, preventing unexpected behavior.

--- src/Components/GalleryComponents/GalleryGrids.jsx
+++ src/Components/GalleryComponents/GalleryGrids.jsx
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 import { Code } from "lucide-react";
 
 const GalleryGrids = ({ Data }) => {
+
   return (
     <section className="mt-6 grid w-full max-w-[1200px] grid-cols-1 place-content-center gap-6 md:grid-cols-2 lg:grid-cols-3">
       {Data?.map((item) => (
@@ -16,7 +17,7 @@ const GalleryGrids = ({ Data }) => {
           <div className="mx-4 mt-2 flex   items-center justify-between ">
             <h1 className=" font-semibold text-white">{item.ComponentName}</h1>
             <button className=" rounded-md bg-Border p-1 text-Logo ">
-              <Link to={Data.isUploaded ? `/gallery/PreviewPage/${item.ComponentName}` : "/ComponentNotFound404"}>
+              <Link to={item.isUploaded ? `/gallery/PreviewPage/${item.ComponentName}` : "/ComponentNotFound404"}>
                 <Code strokeWidth="3" />
               </Link>
             </button>
