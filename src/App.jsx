ebaf9fed35bd60b55ccad3a4ded0c5b24a9122e0
Ayusht777 <talesarayush@gmail.com>
2024-07-07 17:10:20 +0530

    feat(app): integrate document title using useDocumentTitle hook and add Helmet for SEO

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import AppRoutes from "@/Routes/AppRoutes";
+import { Helmet } from 'react-helmet';
+import  useDocumentTitle  from '@/Hooks/useDocumentTitle'
 function App() {
+  const title = useDocumentTitle();
   return (
     <>
+      <Helmet>
+        <title>{title}</title>
+        <link rel="icon" type="image/svg+xml" href="/BrandLogo.svg" />
+      </Helmet>
       <AppRoutes></AppRoutes>
-     
+
     </>
   );
 }
