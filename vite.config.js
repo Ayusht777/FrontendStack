09f73ffa2db92899f0d30c981a40e4cdb209a85a
Ayusht777 <talesarayush@gmail.com>
2024-07-08 14:02:25 +0530

    vite update

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path"; // Ensure path module is imported
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
@@ -8,11 +8,11 @@ export default defineConfig({
     alias: {
       "@": path.resolve(__dirname, "./src/"), // Alias for src directory
       Components: path.resolve(__dirname, "./src/Components/"), // Alias for Components
-      public: path.resolve(__dirname, "./public/"), // Alias for public directory
+      Public: path.resolve(__dirname, "./public/"), // Alias for public directory
       Pages: path.resolve(__dirname, "./src/Pages/"), // Alias for Pages
       Assets: path.resolve(__dirname, "./src/Assets/"), // Alias for Assets
       Hooks: path.resolve(__dirname, "./src/Hooks/"), // Alias for Hooks
-      types: path.resolve(__dirname, "./src/@types/"), // Alias for @types
+      Types: path.resolve(__dirname, "./src/@types/"), // Alias for @types
     },
   },
   build: {
