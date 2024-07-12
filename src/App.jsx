71a7cbb2b76037b9aa9f5a8edc59775c86ba993a
Ayusht777 <talesarayush@gmail.com>
2024-07-12 22:57:46 +0530

    feat(app): add additional Helmet metadata and preconnect/preload links for better performance and accessibility

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
 import AppRoutes from "@/Routes/AppRoutes";
 import { Helmet } from 'react-helmet';
-// import useDocumentTitle from "./Hooks/useDocumentTitle";
+
 function App() {
-  // const title = useDocumentTitle();
   return (
     <>
       <Helmet>
-        <title>{"FrontendStack"}</title>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="color-scheme" content="dark" />
+        <title>FrontendStack</title>
         <link rel="icon" type="image/svg+xml" href="/BrandLogo.svg" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link
+          rel="preload"
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap"
+          as="style"
+          onLoad="this.onload=null;this.rel='stylesheet'"
+        />
       </Helmet>
-      <AppRoutes></AppRoutes>
-
+      <AppRoutes />
     </>
   );
 }
