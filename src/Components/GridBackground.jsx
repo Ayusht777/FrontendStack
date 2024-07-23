6a17aae3bcdccf73e87592996890a5c4366940b8
Ayusht777 <talesarayush@gmail.com>
2024-07-23 12:17:35 +0530

    refactor(grid-background): remove unnecessary console log statement
    - Removed the console log statement for children prop in the GridBackground component
    - Cleaned up the code by eliminating unused console logs
    
    This change refines the component by removing unnecessary console output, improving code cleanliness and performance.

--- src/Components/GridBackground.jsx
+++ src/Components/GridBackground.jsx
@@ -1,5 +1,5 @@
-const GridBackground = ({children}) => {
-  console.log(children)
+const GridBackground = ({ children }) => {
+
   return (
     <div className=" relative  box-border size-full  overflow-hidden rounded-3xl outline outline-1  outline-Border aspect-square">
       <div className="absolute inset-0 bg-Nav bg-[linear-gradient(to_right,#404040_1px,transparent_1px),linear-gradient(to_bottom,#404040_1px,transparent_1px)] bg-[size:48px_48px] "></div>
